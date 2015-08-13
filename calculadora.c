137175b9549f1db624cd54cba9ebf63949e5a49e
test <a@b.com>
2015-08-13 12:28:04 -0500

    calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "calculadora.h"
+
+
+int suma (int a,int b)
+{
+return a+b;
+}
+
+int resta (int a,int b)
+{
+return a-b;
+}
+
+int multiplicacion (int a,int b)
+{
+return a*b;
+}
+
+int division (int a,int b)
+{
+return a/b;
+}
