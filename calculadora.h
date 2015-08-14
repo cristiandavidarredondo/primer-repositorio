e84a747c38cb455d10cb5797a6d11a4ccfeafe18
test <a@b.com>
2015-08-14 18:05:29 -0500

    ultimas modificaciones, se tabulo y se comentaron las funciones

---
+++ calculadora.h
@@ -0,0 +1,33 @@
+
+
+/**
+* \brief funcion que suma
+* \primer valor ingresado valor1
+* \segundo valor ingresado  valor2
+* \return  retorna un entero con el resultado
+*/
+int suma (int valor1,int valor2);
+
+/**
+* \brief funcion que resta
+* \primer valor ingresado valor1
+* \segundo valor ingresado  valor2
+* \return  retorna un entero con el resultado
+*/
+int resta (int valor1,int valor2);
+
+/**
+* \brief funcion que multiplica
+* \primer valor ingresado valor1
+* \segundo valor ingresado  valor2
+* \return  retorna un entero con el resultado
+*/
+int multiplicacion (int valor1,int valor2);
+
+/**
+* \brief funcion que divide
+* \primer valor ingresado valor1
+* \segundo valor ingresado  valor2
+* \return  retorna un valor flotante por que el resultado de la division puede se de punto flotante
+*/
+float division (int valor1,int valor2);
